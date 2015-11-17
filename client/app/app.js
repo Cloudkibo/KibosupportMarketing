0ae4c28e6cfb866c497db29001a0f2f1bf25481b
sojharo <sojharo@gmail.com>
2015-11-18 01:34:37 +0500

    website separated
    Signed-off-by: sojharo <sojharo@gmail.com>

--- client/app/app.js
+++ client/app/app.js
@@ -14,4 +14,15 @@ angular.module('kiboMktApp', [
       });
 
     $locationProvider.html5Mode(true);
+  })
+
+  .controller('AppController', function($scope) {
+    $scope.$on('$viewContentLoaded', function() {
+      Metronic.initComponents(); // init core components
+      Layout.init();
+      //Layout.initOWL();
+      RevosliderInit.initRevoSlider();
+
+      //Layout.init(); //  Init entire layout(header, footer, sidebar, etc) on page load if the partials included in server side instead of loading with ng-include directive
+    });
   });
