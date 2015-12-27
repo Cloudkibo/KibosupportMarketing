43b308de5c0b66eb603d76860c9c9f4afc247a45
sabachanna <shabir.saba@gmail.com>
2015-12-28 02:17:05 +0500

    thank you page 246

--- client/app/thankyou/thankyou.controller.js
+++ client/app/thankyou/thankyou.controller.js
@@ -1,26 +1,8 @@
 'use strict';
 
 angular.module('kiboMktApp')
-  .controller('ThankYouCtrl', function ($scope, $http) {
+  .controller('ThankYouCtrl', function ($scope) {
     $scope.message = 'Hello';
 
-    $scope.giveFeedback = function (){
-      $scope.data.subject = 'KiboSupport: Feedback given on KiboSupport Marketing Website';
-      $http.post('https://api.cloudkibo.com/feedback', $scope.data)
-        .success(function(data){
-          $scope.addAlert('success', 'Thank you for your feedback.')
-        })
-    }
-
-    $scope.alerts = [];
-
-    $scope.addAlert = function(newtype, newMsg) {
-      //console.log('Error', newtype, newMsg)
-      $scope.alerts.push({type: newtype, msg: newMsg});
-    };
-
-    $scope.closeAlert = function(index) {
-      $scope.alerts.splice(index, 1);
-    };
 
   });
